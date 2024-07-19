85d19a9de5bc652fb3934f26cbe751ea39ed5dfc
Kyle McLaughlin <k.mclaugh@outlook.com>
2024-07-19 16:54:13 -0400

    Select properties of users to export
    - Added properties selector for choosing ADUser properties to include in exported CSV file

--- Export-ADUsers.ps1
+++ Export-ADUsers.ps1
@@ -2,7 +2,7 @@
 <##
  ##  Author: Kyle McLaughlin
  ##
- ##  Wrote this script to export data for user accounts from our Active Directory.
+ ##  Export data for user accounts from Active Directory.
  ##     Exports the data as a CSV file, for easy use in spreadsheet editors or for other data processing.
  ##
  ##  Written on PowerShell Version 4
@@ -43,7 +43,41 @@ Function GetDeepestOrgUnit([String]$FullCanonicalName)
 
 <#
     .SYNOPSIS
-    Export active desired users including all properties
+    Build string of properties to select from ADUser object when exporting.
+
+    .PARAMS
+    $PropSelector can be: ALL, VERBOSE, NORMAL, MINIMAL,
+                          or you can enter specific properties comma separated (Example: 'CanonicalName, Name, LastLogonDate')
+#>
+Function CreatePropertiesSelector([String]$PropSelector)
+{
+    If ($PropSelector -IEQ 'ALL')
+    {
+        Return '*'
+    }
+    ElseIf ($PropSelector -IEQ 'VERBOSE')
+    {
+        Return 'Created', 'ObjectClass', 'ObjectGUID', 'objectSid', 'MemberOf', 'CanonicalName', 'SAMAccountName', 'Name', 'DisplayName', 'GivenName', 'Initials', 'OtherName', 'Description', 'Title', 'Enabled', 'LockedOut', 'HomeDirectory', 'HomeDrive', 'ScriptPath', 'PasswordExpired', 'PasswordNeverExpires', 'PasswordNotRequired', 'CannotChangePassword', 'lastLogoff', 'lastLogon', 'LastLogonDate', 'lastLogonTimestamp'
+    }
+    ElseIf ($PropSelector -IEQ 'NORMAL')
+    {
+        Return 'Created', 'SAMAccountName', 'Name', 'DisplayName', 'Description', 'LastLogonDate'
+    }
+    ElseIf ($PropSelector -IEQ 'MINIMAL')
+    {
+        Return 'SAMAccountName', 'Name', 'LastLogonDate'
+    }
+    Else
+    {
+        # Split custom properties into array
+        $PropSelector = $PropSelector -Replace '\s',''
+        Return $PropSelector -Split ','
+    }
+}
+
+<#
+    .SYNOPSIS
+    Export users with properties from Active Directory as a CSV file.
 
     .PARAMS
     $OrgUnit                 - Export users from this organization unit
@@ -53,8 +87,11 @@ Function GetDeepestOrgUnit([String]$FullCanonicalName)
 
     $FilterOnlyActiveUsers   - If $true: only include 'Enabled' accounts, and accounts with a last logon less than 180 days ago
                              - If $false: include all users
+
+    $LogVerbosity            - Verbosity of the exported CSV file. (how many properties are included)
+                               See CreatePropertiesSelector function
 #>
-Function ExportOUUsers([String]$OrgUnit, [Boolean]$IncludeNestedOrgUnits, [Boolean]$FilterOnlyActiveUsers)
+Function ExportOUUsers([String]$OrgUnit, [Boolean]$IncludeNestedOrgUnits, [Boolean]$FilterOnlyActiveUsers, [String]$LogVerbosity)
 {
     # Base search filter, for domain and org unit
     [String]$SearchBaseFilter = "OU=$OrgUnit,DC=com,DC=org,DC=local"
@@ -77,13 +114,17 @@ Function ExportOUUsers([String]$OrgUnit, [Boolean]$IncludeNestedOrgUnits, [Boole
 
     # Date variable used in CSV file name
     $FormattedDate = Get-Date -f yyyyMMddhhmm
+    $LogVer = "Log$LogVerbosity"
     $IncludedUsers = "$(If($FilterOnlyActiveUsers){'Active'}Else{'All'})Users"
-    $CSVFile = "$OUExportsDir\$IncludedUsers-$FormattedDate.csv"
+    $CSVFile = "$OUExportsDir\$LogVer-$IncludedUsers-$FormattedDate.csv"
     
+    # The properties to select out of each ADUser object
+    $ExportProperties = $(CreatePropertiesSelector -PropSelector $LogVerbosity)
+
     # Get the users from AD and export as CSV
     Get-ADUser -SearchBase $SearchBaseFilter -Filter $(If ($FilterOnlyActiveUsers) { $ActiveUserFilter } Else { '*' }) -Properties * |
         Where-Object { $IncludeNestedOrgUnits -Or $(GetDeepestOrgUnit -FullCanonicalName $_.CanonicalName) -EQ $OrgUnit } |
-        Select-Object Name, SAMAccountName, Description, LastLogonDate |
+        Select-Object $ExportProperties |
         Sort-Object 'Created' |
         Export-CSV -Path $CSVFile -Encoding UTF8 -NoTypeInformation
 }
